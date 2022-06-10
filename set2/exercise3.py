570706a80683dfe441318d048f6f0128a89182d8
Aaro Lahtinen <lahtinenaaro77@gmail.com>
2022-06-10 19:35:08 +1000

    coordinates done

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -208,6 +208,14 @@ def loops_5():
         f"There are {num_bottles} green bottles"
     you'll come to see the pros and cons of each over time.
     """
+    constant = []
+    for x in range(10):
+        changing = []
+        for y in range(5):
+            changing.append(f"(i{str(x)}, j{str(y)})")
+        constant.append(changing)
+    return constant
+    
 
 
 
